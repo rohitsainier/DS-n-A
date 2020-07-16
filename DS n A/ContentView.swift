f9db5162630eed93bd393af0716d3792a597c551
Rohit Saini <sketchlearn7@gmail.com>
2020-07-17 00:19:01 +0530

    LinkList Push,append,insert after method added

--- DS n A/ContentView.swift
+++ DS n A/ContentView.swift
@@ -6,16 +6,41 @@
 //  Copyright © 2020 AccessDenied. All rights reserved.
 //
 
+import UIKit
+import Foundation
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+class ContentView: UIViewController {
+    override func viewDidLoad() {
+    
+//        let node1 = Node(value: "Rohit")
+//        let node2 = Node(value: "Vikas")
+//        let node3 = Node(value: "Anish")
+//        node1.next = node2
+//        node2.next = node3
+//        print(node1)
+        
+//        var list = LinkedList<Int>()
+//        list.push(1)
+//        list.push(2)
+//        list.push(3)
+//        print(list)
+        
+//        var list = LinkedList<String>()
+//        list.append("Rohit")
+//        list.append("Saini")
+//        list.append("Anish Saini")
+//        print(list)
+//
+//        var list = LinkedList<Int>()
+//        list.append(1)
+//        list.append(2)
+//        list.append(3)
+//        var middleNode = list.node(at: 1)!
+//        middleNode = list.insert(10, after: middleNode)
+//        print(list)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
+
---
+++ DS n A/LinkedList/Node.swift
@@ -0,0 +1,85 @@
+//
+//  Node.swift
+//  DS n A
+//
+//  Created by Rohit Saini on 16/07/20.
+//  Copyright © 2020 AccessDenied. All rights reserved.
+//
+
+import Foundation
+
+public class Node<Value>{
+    public var value:Value
+    public var next:Node?
+    init(value:Value,next:Node? = nil){
+        self.value = value
+        self.next = next
+    }
+}
+
+extension Node:CustomStringConvertible{
+    public var description: String{
+        guard let next = next else{
+            return "\(value)"
+        }
+        return "\(value) -> " + String(describing: next) + " "
+    }
+}
+
+
+//MARK:- LinkedList
+public struct LinkedList<Value>{
+    public var head:Node<Value>?
+    public var tail:Node<Value>?
+    
+    public init(){}
+    public var isEmpty:Bool{
+        return head == nil
+    }
+    
+    public mutating func push(_ value:Value){
+        head = Node(value: value, next: head)
+        if tail == nil{
+            tail = head
+        }
+    }
+    public mutating func append(_ value:Value){
+        guard !isEmpty else{
+            push(value)
+            return
+        }
+        tail!.next = Node(value: value)
+        tail = tail!.next
+    }
+    
+    public func node(at index:Int) -> Node<Value>?{
+        var currentNode = head
+        var currentIndex = 0
+        while currentNode != nil && currentIndex < index{
+            currentNode = currentNode!.next
+            currentIndex = currentIndex + 1
+        }
+        return currentNode
+    }
+    
+    public mutating func insert(_ value:Value ,after node: Node<Value>) -> Node<Value>{
+        guard tail !== node else{
+            append(value)
+            return tail!
+        }
+        node.next = Node(value: value, next: node.next)
+        return node.next!
+    }
+}
+
+extension LinkedList:CustomStringConvertible{
+    public var description: String{
+        guard let head = head else{
+            return "No list"
+        }
+        return String(describing: head)
+    }
+}
+
+
+
